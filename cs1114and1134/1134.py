8c79584113b8b65c1e35c2b8823f57c9eff69379
123R3N321 <jr5887@nyu.edu>
2024-08-08 14:55:20 -0400

    Add question based on summer 2024 midterm

---
+++ cs1114and1134/1134.py
@@ -0,0 +1,41 @@
+'''
+summer 2024 midterm 2234 recursion problem:
+count the number of ways you can represent a number as
+sum of 1s and 2s
+'''
+def recur(num):
+    if 2 > num:
+        return 1
+    return recur(num-1) + recur(num-2)
+
+'''
+dp improvement, by chatgpt
+'''
+def recur(num, memo={}):
+    if num in memo:
+        return memo[num]
+    if num < 2:
+        return 1
+
+    memo[num] = recur(num-1, memo) + recur(num-2, memo)
+    return memo[num]
+
+'''
+alternative of non recursion
+'''
+
+
+def count_ways(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
